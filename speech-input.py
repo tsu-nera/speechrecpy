d4b078febd3981451b47687222ec2b48a811e6ea
Tsunemichi Harada <fox10225fox@gmail.com>
2018-08-28 22:54:57 +0900

    maruten

--- speech-input.py
+++ speech-input.py
@@ -2,12 +2,29 @@ import speech_recognition as sr
 import pyperclip
 import pyautogui
 
-r = sr.Recognizer()
-mic = sr.Microphone()
+def check_maruten(text):
+    if text == "まる":
+        return "。"
+    elif text == "10":
+        return "、"
+    elif text == "改行":
+        return "\n"
+    else:
+        return text
 
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
+def main():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
 
-pyperclip.copy(r.recognize_google(audio, language='ja-JP'))
-pyautogui.hotkey('ctrl', 'V')
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    text = r.recognize_google(audio, language='ja-JP')
+    text = check_maruten(text)
+
+    pyperclip.copy(text)
+    pyautogui.hotkey('ctrl', 'V')
+
+###################
+main()
